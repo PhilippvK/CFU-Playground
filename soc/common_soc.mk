e8f940e39d47fc71b0c1fab2f9cb7f1bf22cc6fd
Philipp van Kempen <philipp.van-kempen@tum.de>
2026-01-21 08:38:51 +0100

    soc/common_soc.mk: expose OUT_DIR

--- soc/common_soc.mk
+++ soc/common_soc.mk
@@ -39,7 +39,7 @@ TARGET_ARGS:= --target $(TARGET)
 SOFTWARE_ARGS:= --software-load --software-path $(PROJ_DIR)/build/software.bin
 
 SOC_NAME:=  $(TARGET).$(PROJ)
-OUT_DIR:=   build/$(SOC_NAME)
+OUT_DIR ?=   build/$(SOC_NAME)
 UART_ARGS=  --uart-baudrate $(UART_SPEED)
 LITEX_ARGS= --output-dir $(OUT_DIR) \
         --csr-json $(OUT_DIR)/csr.json \
